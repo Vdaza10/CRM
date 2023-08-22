64f1fe489b2ca42c6cb6cf9c6800203031890dc0
Velki Daza <vdaza@ulearning.com.co>
2023-08-22 08:22:27 -0500

    configuracion de api registro

--- Vyhon/Back-end/src/index.js
+++ Vyhon/Back-end/src/index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import router from "./rutas/totals.routes.js";
+import cors from "cors"
 
 
 const app = express();
 
 
-//ruta index//
-app.use(router)
 
 
 app.use(express.json());
-
+app.use(cors())
 app.listen(3005, () => {
     console.log('Servidor corriendo en puerto 3005')
 })
 
+//ruta index//
+app.use(router)
 
